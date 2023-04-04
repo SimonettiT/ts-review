e5f13c0dd3aab1830cc0babf32bb1eca1e87ce5d
SimonettiT <contacto@simonettitomas.com>
2023-04-04 08:22:47 -0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<script setup></script>
+<template>
+	<RouterView />
+</template>
+
+<style lang="sass">
+@use "@/style/colors"
+@use "@/style/fonts"
+
+*
+    box-sizing: border-box
+    margin: 0
+    padding: 0
+#app
+    font-family: 'Plus Jakarta Sans', sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    line-height: 1.5
+body
+    background-color: colors.$background
+    color: colors.$text
+    overflow: overlay
+
+:root
+    color-scheme: dark
+.container
+    max-width: 1280px
+    width: 95%
+    margin-inline: auto
+
+img
+    max-width: 100%
+h1
+    font-size: fonts.$font-xxxl
+h2
+    font-size: fonts.$font-xxl
+h3
+    font-size: fonts.$font-xl
+h4
+    font-size: fonts.$font-lg
+h5
+    font-size: fonts.$font-base
+h6
+    font-size: fonts.$font-sm
+p, a
+    font-size: fonts.$font-base
+//  Scrollbar
+// TODO: - Make it work
+*
+    scrollbar-width: auto
+    scrollbar-color: blue
+*::-webkit-scrollbar
+    width: 0.7rem
+    background: transparent
+*::-webkit-scrollbar-thumb
+    background-color: lightblue
+    border-radius: 10px
+    &:hover
+        background-color: red
+
+
+//   When the modal is open, the body isn't scrollable.
+//   This style is marked as !important because otherwise it is nullified by the default element style
+
+#modal-open
+    overflow: hidden !important
+
+
+
+.markdown-text
+    line-height: 1.7
+    text-align: left
+    *
+        margin-block: 1.5rem
+    p, a, span
+        font-size: fonts.$font-md
+    a
+        color: colors.$primary
+        text-decoration: underline
+    br
+        display: block
+        content: ""
+        margin-top: 0.7em
+        line-height: 200%
+        vertical-align: top
+    h1, h2, h3, h4, h5, h6
+        margin-block: 2rem 1.5rem
+    li
+        margin-block: 0.5rem
+    img
+        width: 100%
+        max-width: 1280px
+        max-height: 700px
+        display: block
+        height: auto
+        margin-inline: auto
+        object-fit: cover
+        object-position: center
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+/*
+ *
+ * 1. https://vumbnail.com/740535996.jpg
+ * 2. https://img.youtube.com/vi/xLu7B_wqqM0/maxresdefault.jpg
+ * 3. https://vumbnail.com/738418687.jpg
+ *
+ */
+</script>
+
+<template>
+	<header></header>
+	<main>
+		<h1 class="introduction__title">Video <span class="introduction__title--accent">Re-View.</span></h1>
+		<p class="introduction__description">Plataforma de revisión de vídeos creados por Simonetti Tomás</p>	
+		<button class="introduction__btn btn">Comenzar</button>	
+	</main>
+</template>
+
+<style lang="sass" scoped>
+@use "@/style/colors"
+.introduction__title--accent
+	color: colors.$primary
+</style>
